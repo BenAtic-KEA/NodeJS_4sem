381cfe4bb04a63ba00e5863f63265938f1531392
BenAtic-KEA <mark024e@stud.kea.dk>
2022-08-29 09:54:54 +0200

    type coersion

---
+++ 0._Miscellaneous/type_coersion.js
@@ -0,0 +1,9 @@
+// type coersion lecture
+const total = 34.32;
+const price = 12.32;
+const sum = total + price;
+
+console.log(total,price,sum);
+
+const balanceEquals = total === sum;
+console.log(balanceEquals);
