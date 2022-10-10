f6d86d4e967d711ce26247d0663c0e090fed6eab
BenAtic-KEA <mark024e@stud.kea.dk>
2022-10-10 10:57:51 +0200

    first svelte

---
+++ 5._First_Svelte_Project/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+    const name = "Mark"
+    const todaysDate = new Date();
+</script>
+
+<h2 id="page-title">First Svelte Project</h2>
+
+<div> More Content for {name}</div>
+<h4>Current Date: {todaysDate}</h4>
+
+<style>
+  #page-title{
+    color:blue
+  }
+</style>
