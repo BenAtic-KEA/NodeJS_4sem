4fa3736142e7fcc6669f4b9014b7784d5669cf87
Mark Hansen <Mark024e@stud.kea.dk>
2022-10-23 13:00:54 +0200

    Reactivity assignments&Declaration done

--- Afleveringer/Svelte_Exercises/svelte/src/App.svelte
+++ Afleveringer/Svelte_Exercises/svelte/src/App.svelte
@@ -2,6 +2,8 @@
   import AddingData from './components/Introduction/adding_data/AddingData.svelte';
   import DynamicAttributes from './components/Introduction/dynamic_attributes/DynamicAttributes.svelte';
   import Styling from './components/Introduction/styling/Styling.svelte';
+  import HtmlTags from './components/Introduction/HTML_tags/HTMLTags.svelte';
+  import Assignments from './components/Reactivity/Assignments/Assignments.svelte';
 </script>
 
 <main>
@@ -13,6 +15,9 @@
 
   <Styling></Styling>
 
+  <HtmlTags></HtmlTags>
+
+  <Assignments></Assignments>
 </main>
 
 <style>
---
+++ Afleveringer/Svelte_Exercises/svelte/src/components/Reactivity/Assignments/Assignments.svelte
@@ -0,0 +1,17 @@
+<script>
+
+let count = 0;
+
+function incrementCount(){
+    count += 1;
+}
+
+
+</script>
+
+<main>
+
+    <button on:click ={incrementCount}>
+        Clicked {count} {count === 1 ? 'time' : 'times'}
+    </button>
+</main>
