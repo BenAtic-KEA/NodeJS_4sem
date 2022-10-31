5e367957d04e3ace61ecdf6aaaef5e849264e2ad
BenAtic-KEA <mark024e@stud.kea.dk>
2022-10-31 09:23:05 +0100

    sent data between classes

--- 6._Svelte_Family/src/components/Child/Child.svelte
+++ 6._Svelte_Family/src/components/Child/Child.svelte
@@ -2,9 +2,15 @@
     import Pet from "../Pet/Pet.svelte";
     export let child;
     export let pets
+    export let onTellILoveYou
+    export let onAlwaysSayHi
+    export let parentName
+
 </script>
 
 <p>{child.name}</p>
+<button on:click={onAlwaysSayHi} >Whisper in parents ear</button>
+<button on:click={onTellILoveYou(`i love you, ${parentName}`  )} >Whisper in parents ear</button>
 {#each pets as pet}
 <Pet pet={pet}></Pet>
 {/each}
--- 6._Svelte_Family/src/components/Parent/Parent.svelte
+++ 6._Svelte_Family/src/components/Parent/Parent.svelte
@@ -3,9 +3,20 @@
     export let name;
     export let children
     export let pets
+    export let parents
+
+// create a function that is called *AlwaysSayHi and make the child always say hi to the parent when greeting
+
+    function handleAlwaysSayHi(){
+        console.log('Hi!')
+    }
+
+    function handleTellILoveYou(message){
+        console.log(message);
+    }
 </script>
 
 <h1>Hi I'm {name}</h1>
 {#each children as child}
-<Child child = {child} pets ={pets}/>
+<Child parentName={name} onAlwaysSayHi={handleAlwaysSayHi} onTellILoveYou = {handleTellILoveYou} child = {child} pets ={pets}/>
 {/each}
